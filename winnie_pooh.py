138ba913308b1fd001d1053eebdee775331ab391
mango420 <60978002+mango420@users.noreply.github.com>
2025-01-22 11:06:42 +0100

    util/logging: added and config of logging

--- winnie_pooh.py
+++ winnie_pooh.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
+import logging
 
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    log.info(msg='Starting the Honeypot')
